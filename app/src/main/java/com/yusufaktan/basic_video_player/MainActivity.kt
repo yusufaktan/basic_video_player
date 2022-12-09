a7a50df283803aa8227e6d491cfb1c9762da8219
Yusuf Aktan <ycan2497@gmail.com>
2022-12-09 17:09:45 +0300

    basic_video_player

---
+++ app/src/main/java/com/yusufaktan/basic_video_player/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.yusufaktan.basic_video_player
+
+import android.net.Uri
+import android.net.Uri.*
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //Find the VideoView class by its id
+        val videoView = findViewById<VideoView>(R.id.testView)
+        //Creating MediaController
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        //specify the location of media file
+        val uri:Uri = parse(
+            "android.resource://" + packageName
+                    + "/raw/testvideo"
+        )
+        //Setting MediaController and URI, then starting the videoView
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
+    }
+}
